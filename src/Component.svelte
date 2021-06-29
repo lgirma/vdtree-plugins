0b50a9b7e18ca6892675a0fa093ece4acc6f378a
Leone <leeman2016@gmail.com>
2021-06-29 23:33:58 +0300

    Initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    //import {AbstractDomElement} from "./AbstractDOM";
+    import {h, renderToDom} from 'vdtree'
+    import {isFunc} from "boost-web-core";
+    import { onMount } from 'svelte';
+
+    export let dom//: AbstractDomElement
+    export let props = null
+    let container;
+    let mounted = false;
+    let domInstance = null;
+
+    onMount(() => {
+        mounted = true;
+    })
+
+    $: {
+        if (mounted) {
+            let _dom = dom
+            if (isFunc(dom))
+                _dom = h(dom)
+            if (props != null)
+                _dom.props = props
+            if (domInstance != null)
+                domInstance.update(_dom)
+            else
+                domInstance = renderToDom(_dom, container)
+        }
+    }
+</script>
+
+<div bind:this={container}>
+    <slot></slot>
+</div>
